0f3e10909e82852c144f9388755025836778fded
Aaron Peschel <apeschel@zendesk.com>
2013-09-26 12:52:29 -0700

    Clean Up Constructors
    Move logic out of constructor to helper method. Should help make the
    logic easier to follow.

--- lib/chef/knife/node_dependency_tree.rb
+++ lib/chef/knife/node_dependency_tree.rb
@@ -12,10 +12,10 @@ class NodeDependencyTree < ::Chef::Knife
     @@cookbook_versions = {}
     @@seen_cookbooks = Set.new
 
-    def initialize(name, children=[])
+    def initialize(name, children=[], color=:red)
       @name = name
       @children = children
-      @color = :red
+      @color = color
     end
 
     def to_s
@@ -36,11 +36,11 @@ class NodeDependencyTree < ::Chef::Knife
       output.join("\n")
     end
 
-    def roles_to_cookbooks(roles)
+    def self.roles_to_cookbooks(roles)
       roles.map { |role_name| RoleNode.new(role_name) }
     end
 
-    def recipes_to_cookbooks(recipes)
+    def self.recipes_to_cookbooks(recipes)
       recipes.map do |recipe|
         recipe.partition("::").first
       end.uniq.map do |cookbook_name|
@@ -51,34 +51,50 @@ class NodeDependencyTree < ::Chef::Knife
 
   class NodeNode < TreeNode
     def initialize(name)
+      @@cookbook_versions = get_cookbook_versions(name)
+      dependencies = get_dependencies(name)
+      color = :green
+      super(name, dependencies, color)
+    end
+
+    def get_cookbook_versions(name)
       node = ::Chef::Node.load(name)
-
       environment = ::Chef::Environment.load(node.chef_environment)
-      @@cookbook_versions = environment.cookbook_versions
+      environment.cookbook_versions
+    end
 
-      cookbooks = recipes_to_cookbooks(node[:recipes])
-      roles = roles_to_cookbooks(node[:roles])
-      super(name, cookbooks + roles)
-
-      @color = :green
+    def get_dependencies(name)
+      node = ::Chef::Node.load(name)
+      cookbooks = TreeNode.recipes_to_cookbooks(node[:recipes])
+      roles = TreeNode.roles_to_cookbooks(node[:roles])
+      cookbooks + roles
     end
   end
 
   class RoleNode < TreeNode
     def initialize(name)
-      role = ::Chef::Role.load(name)
-      cookbooks = recipes_to_cookbooks(role.run_list.recipes)
-      roles = roles_to_cookbooks(role.run_list.roles)
-      super(name, cookbooks + roles)
+      dependencies = get_dependencies(name)
+      color = :cyan
+      super(name, dependencies, color)
+    end
 
-      @color = :cyan
+    def get_dependencies(name)
+      role = ::Chef::Role.load(name)
+      cookbooks = TreeNode.recipes_to_cookbooks(role.run_list.recipes)
+      roles = TreeNode.roles_to_cookbooks(role.run_list.roles)
+      cookbooks + roles
     end
   end
 
   class CookbookNode < TreeNode
     def initialize(name)
       @@seen_cookbooks.add(name)
-      # XXX: Clean this up.
+      dependencies = get_dependencies(name)
+      color = :yellow
+      super(name, dependencies, color)
+    end
+
+    def get_dependencies(name)
       cookbook_version = @@cookbook_versions[name]
       cookbooks = []
       if cookbook_version
@@ -91,9 +107,8 @@ class NodeDependencyTree < ::Chef::Knife
           CookbookNode.new(cookbook_name)
         end
       end
-      super(name, cookbooks)
 
-      @color = :yellow
+      return cookbooks
     end
   end
 
